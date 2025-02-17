2035d03c5caebec5093bd2693d8a4da1b7a50fe2
omgithe <a.s.chelakhov@icloud.com>
2025-02-17 19:55:14 +0300

    add lib asio

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -1,10 +1,15 @@
 #include "GameInstance.hpp"
 
-int main(void)
-{
-    GameInstance gi;
+#include <thread>
+#include <chrono>
 
-    gi.Run();
+int main(void) {
+	GameInstance gi;
 
-    return 0;
+	gi.Run();
+
+	std::cout << "Exit" << std::endl;
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	return 0;
 }
--- src/client/GameInstance.cpp
+++ src/client/GameInstance.cpp
@@ -1,28 +1,22 @@
 #include "GameInstance.hpp"
 
-GameInstance::GameInstance()
-{
+GameInstance::GameInstance() {
 
 }
 
-GameInstance::~GameInstance()
-{
+GameInstance::~GameInstance() {
 	glfwTerminate();
 }
 
-void GameInstance::Run()
-{
-	if (!glfwInit())
-		exit(-1);
+void GameInstance::Run() {
 
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	if (!glfwInit()) {
+		exit(-1);
+	}
 
 	window = glfwCreateWindow(640, 480, "Wing Client", NULL, NULL);
 
-	if (!window)
-	{
+	if (!window) {
 		std::cout << "glfwCreateWindow failed" << std::endl;
 		glfwTerminate();
 		exit(-1);
@@ -30,8 +24,7 @@ void GameInstance::Run()
 
 	glfwMakeContextCurrent(window);
 
-	if (!gladLoadGL())
-	{
+	if (!gladLoadGL()) {
 		std::cout << "gladLoadGL failed" << std::endl;
 		exit(-1);
 	}
@@ -42,8 +35,7 @@ void GameInstance::Run()
 	glfwSwapInterval(1);
 
 	glClearColor(0, 1, 0, 1);
-	while (!glfwWindowShouldClose(window))
-	{
+	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
